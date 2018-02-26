763c2cd20e2a447d0d7e1183826a119d2d14856b
allenzhong2015 <36616412+allenzhong2015@users.noreply.github.com>
2018-02-26 04:56:23 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -116,11 +116,8 @@ jQuery(document).ready(function($){
 		this.modal.attr('data-event', event.parent().attr('data-event'));
 
 		//update event content
-		this.modalBody.find('.event-info').load(event.parent().attr('data-content')+'.html .event-info > *', function(data){
-			//once the event content has been loaded
-			self.element.addClass('content-loaded');
-		});
-
+		this.modalBody.find('.event-info').html(event.find('.test').html());
+		
 		this.element.addClass('modal-is-open');
 
 		setTimeout(function(){
